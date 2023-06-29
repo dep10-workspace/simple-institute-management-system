e1f8e97023592b3d4cdbf8028fe120b894d51b16
danushka kasun <danushkakasun05@gmail.com>
2023-06-29 21:44:23 +0530

    finalizing error handling

--- nodejs-app/src/api/courseHttpController.ts
+++ nodejs-app/src/api/courseHttpController.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {dataSourse} from "../db/dbConnector";
+console.log("HTTPcONTROLLER")
 
 export const router =express.Router();
 router.get('/',async (req,res)=>{
@@ -8,13 +9,34 @@ router.get('/',async (req,res)=>{
 });
 
 router.post('/',async (req,res)=>{
+    try {
     const course = req.body as course;
-    const result = await dataSourse.query("insert into course (id,description,duration) values (?,?,?)",[course.id,course.description,course.duration]);
-    res.sendStatus(201);
+    if(!/^C\d{3}$/.test(course.id)|| !/^[A-z ]{3,}$/.test(course.description) || !course.duration){
+        res.status(404).send('Invalid input');
+        return;
+    }
+        const result = await dataSourse.query("insert into course (id,description,duration) values (?,?,?)", [course.id, course.description, course.duration]);
+        if (result.affectedRows === 1) {
+            res.sendStatus(201);
+            return;
+        }
+        res.sendStatus(404);
+    }catch (err:any){
+        res.status(404).send("Invalid Data Entry");
+    }
 })
 router.delete('/:courseId',async (req,res)=>{
+    if(!/^C\d{3}$/.test(req.params.courseId)){
+        console.log("123")
+        res.sendStatus(404).send("Invalid data input");
+        return;
+    }
     const result = await dataSourse.query("delete from course where id=?",[req.params.courseId]);
-    res.sendStatus(204);
+    if(result.affectedRows===1){
+        res.sendStatus(204);
+        return;
+    }
+    res.sendStatus(400);
 })
 
 
--- nodejs-app/src/main.ts
+++ nodejs-app/src/main.ts
@@ -1,9 +1,13 @@
-import express from 'express'
+import express, {json} from 'express'
 import { router as courseRouter} from './api/courseHttpController'
+import cors from 'cors'
+console.log("MAIN")
 
 
 const app =express();
-app.listen('8081',()=>console.log("server has started"));
+app.listen('8080',()=>console.log("server has started"));
+app.use(cors());
+app.use(json())
 app.use('/api/v1/courses',courseRouter);
 
 
