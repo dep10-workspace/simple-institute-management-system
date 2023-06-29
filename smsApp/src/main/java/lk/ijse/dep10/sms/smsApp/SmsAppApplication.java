ac6cf67ca75acf2dd3f6ee4395aba8f14485e486
Subhash-Chamara <udayanga.subash@gmail.com>
2023-06-29 19:17:31 +0530

    Setup The Project Structure

---
+++ smsApp/src/main/java/lk/ijse/dep10/sms/smsApp/SmsAppApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.sms.smsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsAppApplication.class, args);
+	}
+
+}
