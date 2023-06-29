47c201d8fb6812be9a60960a9a4d327986a085d4
Dilan Asiri <dilanasiri95@gmail.com>
2023-06-29 19:54:45 +0530

    Create a UI in Manage Student

--- student-ui/src/app/app.module.ts
+++ student-ui/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ManageStudentComponent } from './view/manage-student/manage-student.component';
 import { ManageCoursesComponent } from './view/manage-courses/manage-courses.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './view/home/home.component';
 
+const router:Routes=[
+  {
+    path:'students',
+    component:ManageStudentComponent
+  },
+  {
+    path:'courses',
+    component:ManageCoursesComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
     ManageStudentComponent,
-    ManageCoursesComponent
+    ManageCoursesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
